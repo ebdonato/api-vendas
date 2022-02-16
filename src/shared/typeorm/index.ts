1e0a8efa787e1daec965295808f5e7fe7f30d4ec
Eduardo Donato <eduardo.donato@gmail.com>
2022-02-16 02:03:49 -0300

    update typerom config env (add environment variables)

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,3 @@
 import { createConnection } from 'typeorm';
 
-createConnection({
-    type: 'postgres',
-    url: 'postgres://postgres:docker@localhost:5432/apivendas',
-    migrations: ['./src/shared/typeorm/migration/*.js'],
-    cli: {
-        migrationsDir: './src/shared/typeorm/migration',
-    },
-});
+createConnection();
