63b0c758bfed9917c9b88c80c2a720a190974b2e
Eduardo Donato <eduardo.donato@gmail.com>
2022-02-17 01:04:08 -0300

    fix error middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -14,7 +15,11 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+app.use(errors());
+
+//é necessário o último argumento não suado para a assinatura da função ser a correta
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, _: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
             status: 'Error',
