9f583ff75a1202d0cfb2019fb0e4b88142af0d2b
Eduardo Donato <eduardo.donato@gmail.com>
2022-02-17 01:04:44 -0300

    add products routes body and params validation

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,14 +1,32 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
-
 const productsController = new ProductsController();
 
+const bodySchema = {
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required(),
+    },
+};
+
+const paramsSchema = {
+    [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+    },
+};
+
 productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.show);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+
+productsRouter.get('/:id', celebrate({ ...paramsSchema }), productsController.show);
+
+productsRouter.post('/', celebrate({ ...bodySchema }), productsController.create);
+
+productsRouter.put('/:id', celebrate({ ...paramsSchema, ...bodySchema }), productsController.update);
+
+productsRouter.delete('/:id', celebrate({ ...paramsSchema }), productsController.delete);
 
 export default productsRouter;
