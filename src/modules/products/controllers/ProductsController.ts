0c4ce6616f8cca2cd5f9efe3c72ffcdcbc120678
Eduardo Donato <eduardo.donato@gmail.com>
2022-02-16 23:42:05 -0300

    create products controller

---
+++ src/modules/products/controllers/ProductsController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import CreateProductService from '../services/CreateProductService';
+import DeleteProductService from '../services/DeleteProductService';
+import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
+import UpdateProductService from '../services/UpdateProductService';
+
+export default class ProductsController {
+    public async index(_: Request, response: Response): Promise<Response> {
+        const service = new ListProductService();
+
+        const products = await service.execute();
+
+        return response.json(products);
+    }
+
+    public async show(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+
+        const service = new ShowProductService();
+
+        const product = await service.execute({ id });
+
+        return response.json(product);
+    }
+
+    public async create(request: Request, response: Response): Promise<Response> {
+        const { name, price, quantity } = request.body;
+
+        const service = new CreateProductService();
+
+        const product = await service.execute({ name, price, quantity });
+
+        return response.json(product);
+    }
+
+    public async update(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+        const { name, price, quantity } = request.body;
+
+        const service = new UpdateProductService();
+
+        const product = await service.execute({ id, name, price, quantity });
+
+        return response.json(product);
+    }
+
+    public async delete(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+
+        const service = new DeleteProductService();
+
+        await service.execute({ id });
+
+        return response.json([]);
+    }
+}
