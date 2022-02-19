b49f77b2513e3341ca5c0ab2103733f24692eeff
Eduardo Donato <eduardo.donato@gmail.com>
2022-02-19 00:38:02 -0300

    fix Products typos

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity('products')
 class Product {
     @PrimaryGeneratedColumn('uuid')
     id: string;
