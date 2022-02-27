d9feac8d22c2ec8573c6d1fec058b7660ac27983
Eduardo Donato <eduardo.donato@gmail.com>
2022-02-27 01:06:07 -0300

    improve redis cache config

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -1,19 +1,9 @@
 import { RedisOptions } from 'ioredis';
 
-interface ICacheConfig {
-    config: {
-        redis: RedisOptions;
-    };
-    driver: string;
-}
+const cacheConfig: RedisOptions = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    password: process.env.REDIS_PASS || undefined,
+};
 
-export default {
-    config: {
-        redis: {
-            host: process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT,
-            pass: process.env.REDIS_PASS || undefined,
-        },
-    },
-    driver: 'redis',
-} as ICacheConfig;
+export default cacheConfig;
--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -5,9 +5,10 @@ export default class RedisCache {
     private client: RedisClient;
 
     constructor() {
-        this.client = new Redis(cacheConfig.config.redis);
+        this.client = new Redis(cacheConfig);
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     public async save(key: string, value: any): Promise<void> {
         await this.client.set(key, JSON.stringify(value));
     }
